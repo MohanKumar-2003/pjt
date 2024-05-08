5ac87f899ec4dd9e6d8f5cf56770222f161941ce
Avanthika0903 <150774721+Avanthika0903@users.noreply.github.com>
2024-05-08 10:44:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "secrets.AWS_ACCESS_KEY_ID"
+  secret_key = "secrets.AWS_SECRET_ACCESS_KEY"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
